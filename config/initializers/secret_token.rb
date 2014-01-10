9088d0406afc5f69ef6f364289f5df9fe76ba6eb
Igor Goldenway <goldenway777@gmail.com>
2014-01-10 15:48:12 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pbook::Application.config.secret_token = '382a34a2b0a37de74a63f9edb4f99e1a0166e1f7dc59d0248d5307bf316f7c04c096c14742ea1665ae79c03926fdbcb965001fd1d6bc7ad6a43a8bb1c61849f4'
